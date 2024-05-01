bd6095c40fc22f31a0f50ffd96be9bdf27166daf
Subhan002 <smohammad2@leomail.tamuc.edu>
2024-05-01 08:20:17 -0500

    sync

--- mainapp.py
+++ mainapp.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 def main():
     return render_template('main.html')
 
-@app.route('/guide')
-def guide():
-    return render_template('guide.html')
+# @app.route('/guide')
+# def guide():
+#     return render_template('guide.html')
 
-@app.route('/about-us')
-def about_us():
-    return render_template('about_us.html')
+# @app.route('/about-us')
+# def about_us():
+#     return render_template('about_us.html')
 
-@app.route('/contact-us')
-def contact_us():
-    return render_template('contact_us.html')
+# @app.route('/contact-us')
+# def contact_us():
+#     return render_template('contact_us.html')
 
 @app.route('/help')
 def help():
